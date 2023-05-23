f40c8f6501c778803516ed52f9310dfbab302f5f
Medhat Mohammed <68431349+standard-librarian@users.noreply.github.com>
2023-05-23 22:42:03 +0300

    Create tableModel.py

---
+++ tableModel.py
@@ -0,0 +1,19 @@
+from PySide6 import QtCore
+from PySide6.QtCore import Qt
+
+
+class TableModel(QtCore.QAbstractTableModel):
+    def __init__(self, data):
+        super(TableModel, self).__init__()
+        self._data = data
+
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            return self._data[index.row()][index.column()]
+
+    def rowCount(self, index):
+        return len(self._data)
+
+    def columnCount(self, index):
+        return len(self._data[0])
+
